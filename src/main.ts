87a8c418cba163dcdc08aacc9739584642db4093
meilcli <ics.sora.meil@gmail.com>
2020-02-07 18:28:23 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as webhooks from "@octokit/webhooks";
 import * as octokit from "@octokit/rest";
 
 interface Option {
+    githubToken: string;
     triggerRegex: string;
     assignLabels: string[] | null;
     assignReviewers: string[] | null;
@@ -12,6 +13,7 @@ interface Option {
 
 function getOption(): Option {
     return {
+        githubToken: core.getInput("github_token", { required: true }),
         triggerRegex: core.getInput("trigger_regex", { required: true }),
         assignLabels: getInputStringArray("assign_labels", false),
         assignReviewers: getInputStringArray("assign_reviewers", false)
@@ -20,29 +22,25 @@ function getOption(): Option {
 
 async function run() {
     try {
-        core.info("start action commenter");
         if (github.context.eventName != "issue_comment") {
-            core.info("not issue comment");
             return;
         }
         const option = getOption();
         const payload = github.context.payload as webhooks.WebhookPayloadIssueComment;
         if ("pull_request" in payload.issue == false) {
-            core.info("not pull request comment");
             return;
         }
-        core.info("do");
         if (payload.comment.body.match(option.triggerRegex) == null) {
-            core.info(`not match \n${payload.comment.body}\n\n${option.triggerRegex}`);
             return;
         }
-        core.info(`token length: ${process.env.GITHUB_TOKEN?.length}`);
-        const rest = new octokit.Octokit({ auth: `token ${process.env.GITHUB_TOKEN}` });
+
+        const rest = new octokit.Octokit({ auth: `token ${option.githubToken}` });
         if (option.assignReviewers != null) {
             rest.pulls.createReviewRequest({
                 owner: payload.repository.owner.login,
                 repo: payload.repository.name,
-                number: payload.issue.number,
+                // eslint-disable-next-line @typescript-eslint/camelcase
+                pull_number: payload.issue.number,
                 reviewers: option.assignReviewers
             });
         }
